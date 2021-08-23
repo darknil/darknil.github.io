1315ab4269742e76d8b933178e1a947eb8a7c39d
alex <45723342+darknil@users.noreply.github.com>
2021-08-23 21:52:34 +0300

    added script for nav points

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
-console.log("hello friend");
+let ScrollElements = document.querySelectorAll(".btn-item");
+let IdArr = [0,840,1400,2200,4000]
+function RemoveBackground(item){
+	item.classList.remove("btn-clicked");
+}
+
+function addBackground(item) {
+	item.classList.add("btn-clicked");
+}
+
+function GetElementIndex(nodelist) {
+for(let e = 0 ; e < nodelist.length ; e++) {
+	nodelist[e].addEventListener('click', function() {
+    window.scrollTo( 0,IdArr[e]);
+  });
+}
+}
+
+function ChangeBackground() {
+	/*change items classes to default*/
+	ScrollElements.forEach(item => {
+	RemoveBackground(item);
+	})
+	/*add class for clicked item*/
+	addBackground(this);
+	/*Move to page item*/
+	GetElementIndex(ScrollElements);
+}
+ScrollElements.forEach(item => {
+	item.addEventListener("click",ChangeBackground);
+})
