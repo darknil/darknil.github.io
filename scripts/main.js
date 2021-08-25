651f8ce8b9f4b6ab1ac6e5520bcfcbda783afd82
alex <45723342+darknil@users.noreply.github.com>
2021-08-25 19:58:49 +0300

    navigation points
    changing navigation points by scrolling page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
+/*Navigation points*/
+
 let ScrollElements = document.querySelectorAll(".btn-item");
-let IdArr = [0,840,1400,2200,4000]
+let IdArr = [0,840,1400,2200,3200,3800]
 function RemoveBackground(item){
 	item.classList.remove("btn-clicked");
 }
@@ -11,8 +13,8 @@ function addBackground(item) {
 function GetElementIndex(nodelist) {
 for(let e = 0 ; e < nodelist.length ; e++) {
 	nodelist[e].addEventListener('click', function() {
-    window.scrollTo( 0,IdArr[e]);
-  });
+	window.scrollTo( 0,IdArr[e]);
+	});
 }
 }
 
@@ -29,3 +31,20 @@ function ChangeBackground() {
 ScrollElements.forEach(item => {
 	item.addEventListener("click",ChangeBackground);
 })
+
+/*Navigation points
+===================================================*/
+function ChangeNavPoint(pageYOffset) {
+	for (let i =0; i <= 5; i++) {
+		if (pageYOffset>=IdArr[i] && pageYOffset<IdArr[i+1]) {
+				ScrollElements.forEach(item => {
+				RemoveBackground(item);
+				})
+				addBackground(ScrollElements.item(i));
+		}
+
+	}
+}
+window.addEventListener('scroll', function() {
+		ChangeNavPoint(pageYOffset);
+});
